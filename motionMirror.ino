d602e0f459d46f3fea8092c26c7749288378656b
Derek Schulman <code@derekisthecoolest.com>
2018-04-12 20:19:22 +0000

    Version 4

--- motionMirror.ino
+++ motionMirror.ino
@@ -1,65 +1,56 @@
-// color swirl! connect an RGB LED to the PWM pins as indicated
-// in the #defines
-// public domain, enjoy!
- 
-#define REDPIN 5
+#define REDPIN 5 
 #define GREENPIN 6
 #define BLUEPIN 3
+#define pirPin 12
+
 #define FADESPEED 5     // make this higher to slow down
 
 
-//mix in PIR Code
-int pirPin = 12;
-int motionDetect= 0; 
+int motionDetect = 0; 
+int r, g, b, a, flash;
 
- 
+
 void setup() {
-  pinMode(REDPIN, OUTPUT);
-  pinMode(GREENPIN, OUTPUT);
-  pinMode(BLUEPIN, OUTPUT);
-  pinMode(pirPin, INPUT);
-}
+    
+    pinMode(REDPIN, OUTPUT);
+    pinMode(GREENPIN, OUTPUT);
+    pinMode(BLUEPIN, OUTPUT);
+    pinMode(pirPin, INPUT);
+
+} 
  
- 
-void loop() {
-int r, g, b;
-motionDetect = digitalRead(pirPin);
-	if (motionDetect == HIGH){
-     
-		  // fade from blue to violet
-		  for (r = 0; r < 256; r++) { 
-			analogWrite(REDPIN, r);
-			delay(FADESPEED);
-		  } 
-		  // fade from violet to red
-		  for (b = 255; b > 0; b--) { 
-			analogWrite(BLUEPIN, b);
-			delay(FADESPEED);
-		  } 
-		  // fade from red to yellow
-		  for (g = 0; g < 256; g++) { 
-			analogWrite(GREENPIN, g);
-			delay(FADESPEED);
-		  } 
-		  // fade from yellow to green
-		  for (r = 255; r > 0; r--) { 
-			analogWrite(REDPIN, r);
-			delay(FADESPEED);
-		  } 
-		  // fade from green to teal
-		  for (b = 0; b < 256; b++) { 
-			analogWrite(BLUEPIN, b);
-			delay(FADESPEED);
-		  } 
-		  // fade from teal to blue
-		  for (g = 255; g > 0; g--) { 
-			analogWrite(GREENPIN, g);
-			delay(FADESPEED*2);
-		  } 
-	}else if (motionDetect == LOW){
-	analogWrite(GREENPIN, 0);
-	analogWrite(BLUEPIN, 0);
-	analogWrite(REDPIN, 0);
-	}
-	
-}
+
+void loop(){  
+    
+    motionDetect = digitalRead(pirPin);
+    
+  if (motionDetect == HIGH){
+      delay(500);
+      if (motionDetect == HIGH){
+        for (r = 0; r < 256; r++) { 
+          analogWrite(REDPIN, r);
+          delay(FADESPEED);
+        }
+        delay(1000);
+        for (a = 0; a < 256; a++) { 
+          analogWrite(GREENPIN, a);
+          analogWrite(BLUEPIN, a);
+          delay(FADESPEED);
+        }
+        delay(5000);
+        
+
+        }
+      else if (motionDetect == LOW){
+          analogWrite(GREENPIN, 0);
+          analogWrite(BLUEPIN, 0);
+          analogWrite(REDPIN, 0);
+      }
+  }else if (motionDetect == LOW){
+  analogWrite(GREENPIN, 0);
+  analogWrite(BLUEPIN, 0);
+  analogWrite(REDPIN, 0);
+  }
+
+} //end Loop()              
+
